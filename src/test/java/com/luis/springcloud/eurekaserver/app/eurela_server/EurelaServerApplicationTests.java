eaf9208aebce06a0a636b36bce40946ad6accabe
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2025-03-09 00:40:39 -0600

    First commit

---
+++ src/test/java/com/luis/springcloud/eurekaserver/app/eurela_server/EurelaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.springcloud.eurekaserver.app.eurela_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurelaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
